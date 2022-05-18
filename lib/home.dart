ec51dfc18c85efd7331677fae07836c127820193
ahmakafi <som2018mali@gmail.com>
2022-05-18 08:35:08 +0300

    clearing code

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,175 @@
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+// import 'Buttons/Textbuttons.dart';
+import 'Buttons/Textbuttons.dart';
+import 'Buttons/buttons.dart';
 
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userInput = '';
+  var answer = '';
+
+// Array of button
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Calculator",
+          style: TextStyle(color: Colors.black, fontSize: 34),
+        ),
+        backgroundColor: const Color.fromARGB(255, 3, 255, 79),
+      ), //AppBar
+      backgroundColor: const Color.fromARGB(95, 8, 87, 1),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userInput,
+                      style: const TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      answer,
+                      style: const TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ]),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  // Clear Button
+                  if (index == 0) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput = '';
+                          answer = '0';
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: const Color.fromARGB(255, 3, 255, 79),
+                      textColor: Colors.black,
+                    );
+                  }
+
+                  // +/- button
+                  else if (index == 1) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: const Color.fromARGB(255, 3, 255, 79),
+                      textColor: Colors.black,
+                    );
+                  }
+                  // % Button
+                  else if (index == 2) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: const Color.fromARGB(255, 3, 255, 79),
+                      textColor: Colors.black,
+                    );
+                  }
+                  // Delete Button
+                  else if (index == 3) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput =
+                              userInput.substring(0, userInput.length - 1);
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: const Color.fromARGB(255, 3, 255, 79),
+                      textColor: Colors.black,
+                    );
+                  }
+                  // Equal_to Button
+                  else if (index == 18) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.orange[700],
+                      textColor: Colors.white,
+                    );
+                  }
+
+                  // other buttons
+                  else {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? const Color.fromARGB(255, 3, 255, 79)
+                          : Colors.white,
+
+                      // textColor: isOperator(buttons[index])
+                      //     ? Colors.bla
+                      //     : Colors.black,
+                    );
+                  }
+                }),
+          ),
+        ],
+      ),
     );
+  }
+
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+// function to calculate the input operation
+  void equalPressed() {
+    var finaluserinput = userInput;
+    finaluserinput = userInput.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
   }
 }
